7d405c6a64911bdddf7eff7d564378a74eb3b11d
mnmpk <mnmpkm@gmail.com>
2023-10-10 12:13:22 +0800

    save some replSetGetStatus calls

--- monitoring-service/src/main/java/com/mongodb/service/MongoDBMonitoringService.java
+++ monitoring-service/src/main/java/com/mongodb/service/MongoDBMonitoringService.java
@@ -35,53 +35,75 @@ public class MongoDBMonitoringService {
     @Value("${app.requiredNoOfHealthyNode}")
     private int requiredNoOfHealthyNode;
 
+    @Value("${app.independentMonitoringMode}")
+    private boolean independentMonitoringMode;
+
     @Autowired
     private MongoClient mongoClient;
 
-    //@Autowired
-    //private MongoTemplate mongoTemplate;
+    // @Autowired
+    // private MongoTemplate mongoTemplate;
 
-    private enum STATE{
+    private enum STATE {
         HEALTHY,
         UNHEALTHY
     }
 
-    @Scheduled(fixedRateString ="${app.interval}")
+    @Scheduled(fixedRateString = "${app.interval}")
     public void writeServiceHealthinessProbe() {
+        if(independentMonitoringMode){
+            Map<STATE, Integer> map = getReplicaSetStatus();
+            if (alertOpen && map.get(STATE.HEALTHY) >= requiredNoOfHealthyNode) {
+                closeAlert();
+            } else if (!alertOpen && map.get(STATE.HEALTHY) < requiredNoOfHealthyNode) {
+                triggerAlert();
+            }
+        }else{
+            if(alertOpen){
+                Map<STATE, Integer> map = getReplicaSetStatus();
+                if (map.get(STATE.HEALTHY) >= requiredNoOfHealthyNode) {
+                    closeAlert();
+                }
+            }
+        }
+    }
+
+    private Map<STATE, Integer> getReplicaSetStatus() {
         try {
             Document doc = mongoClient.getDatabase("admin").runCommand(new Document("replSetGetStatus", 1));
-            //Document doc = mongoTemplate.getDb().runCommand(new Document("replSetGetStatus", 1));
-            Map<STATE,Integer> map = new HashMap<>();
+            // Document doc = mongoTemplate.getDb().runCommand(new Document("replSetGetStatus", 1));
             List<Document> documents = doc.getList("members", Document.class);
-            primaryOpTime = documents.stream().filter(m->m.getInteger("state")==1).map(m->m.get("optime", Document.class).get("ts", BsonTimestamp.class)).findFirst().get().getTime();
+            primaryOpTime = documents.stream().filter(m -> m.getInteger("state") == 1)
+                    .map(m -> m.get("optime", Document.class).get("ts", BsonTimestamp.class)).findFirst().get()
+                    .getTime();
+            Map<STATE, Integer> map = new HashMap<>();
             for (Document member : documents) {
                 int ts = member.get("optime", Document.class).get("ts", BsonTimestamp.class).getTime();
                 int lagTime = ts - primaryOpTime;
-                if (ts == 0){
+                if (ts == 0) {
                     logger.debug(member.getString("name") + " is not reachable");
-                }
-                else{
+                } else {
                     logger.debug(member.getString("name")
                             + (member.getInteger("state") == 1 ? "(primary)" : (" replication lag:" + lagTime)));
                 }
                 if (Math.abs(lagTime) > replicationLagThershold)
-                    map.put(STATE.UNHEALTHY,map.get(STATE.UNHEALTHY)!=null?(map.get(STATE.UNHEALTHY)+1):1);
+                    map.put(STATE.UNHEALTHY, map.get(STATE.UNHEALTHY) != null ? (map.get(STATE.UNHEALTHY) + 1) : 1);
                 else
-                    map.put(STATE.HEALTHY,map.get(STATE.HEALTHY)!=null?(map.get(STATE.HEALTHY)+1):1);
+                    map.put(STATE.HEALTHY, map.get(STATE.HEALTHY) != null ? (map.get(STATE.HEALTHY) + 1) : 1);
             }
-            if (alertOpen && map.get(STATE.HEALTHY)>=requiredNoOfHealthyNode) {
-                logger.info("Resume normal");
-                alertOpen = false;
-            }else if(!alertOpen && map.get(STATE.HEALTHY)<requiredNoOfHealthyNode){
-                triggerAlert();
-            }
+            return map;
         } catch (MongoTimeoutException ex) {
             logger.error("Write serice unavailable.");
         }
+        return Map.of(STATE.UNHEALTHY, 0, STATE.HEALTHY, 0);
     }
 
-    public void triggerAlert(){
+    public void triggerAlert() {
         logger.warn("Cluster is not healthy/Replication lag exceeds thershold");
         alertOpen = true;
+    }
+    public void closeAlert() {
+        logger.info("Resume normal");
+        alertOpen = false;
     }
 }
